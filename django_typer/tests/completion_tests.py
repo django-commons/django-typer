8d192f010888a5e226eab59de3a82ae37c874fe0
Brian Kohan <bckohan@gmail.com>
2024-02-05 18:52:04 -0800

    run black

--- django_typer/tests/completion_tests.py
+++ django_typer/tests/completion_tests.py
@@ -209,7 +209,7 @@ class BashShellTests(_DefaultCompleteTestCase, TestCase):
     directory = Path("~/.bash_completions").expanduser()
 
     def set_environment(self, fd):
-        #super().set_environment(fd)
+        # super().set_environment(fd)
         os.write(fd, f"export PATH={Path(sys.executable).parent}:$PATH\n".encode())
         os.write(
             fd,
