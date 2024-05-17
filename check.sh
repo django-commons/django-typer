0bd5a370b4f91e76c207ba99d24a813171481802
Brian Kohan <bckohan@gmail.com>
2024-05-16 19:39:54 -0700

    fix linting

--- check.sh
+++ check.sh
@@ -1,7 +1,7 @@
-poetry run ruff format django_typer
+poetry run ruff format
 poetry run ruff format --line-length 80 django_typer/examples
-poetry run ruff check --fix --select I django_typer
-poetry run ruff check --fix django_typer
+poetry run ruff check --fix --select I
+poetry run ruff check --fix
 poetry run mypy django_typer
 pyright
 poetry check
--- manage.py
+++ manage.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+
 import os
 import sys
 
+
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_typer.tests.settings.base')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_typer.tests.settings.base")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,5 +19,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
