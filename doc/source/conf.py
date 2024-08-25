d2ed8ef8066ffa9eaba0115d97cb832a191eef6e
Brian Kohan <bckohan@gmail.com>
2024-08-25 10:56:50 -0700

    remove rtd theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -49,7 +49,6 @@ release = django_typer.__version__
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_rtd_theme',
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinxcontrib.typer',
