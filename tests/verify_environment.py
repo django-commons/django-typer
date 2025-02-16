6e1fbf92f4ee793ad72e10d1050305ee336641f2
Brian Kohan <bckohan@gmail.com>
2025-02-15 20:38:11 -0800

    verify run environment in CI

---
+++ tests/verify_environment.py
@@ -0,0 +1,23 @@
+import os
+import sys
+from django import VERSION
+
+
+def test():
+    # verify that the environment is set up correctly - this is used in CI to make
+    # sure we're testing against the dependencies we think we are
+
+    expected_python = os.environ["TEST_PYTHON_VERSION"]
+    expected_django = os.environ["TEST_DJANGO_VERSION"]
+
+    expected_python = tuple(int(v) for v in expected_python.split(".") if v)
+    assert sys.version_info[: len(expected_python)] == expected_python, (
+        f"Python Version Mismatch: {sys.version_info[: len(expected_python)]} != "
+        f"{expected_python}"
+    )
+
+    expected_django = tuple(int(v) for v in expected_django.split(".") if v)
+    assert VERSION[: len(expected_django)] == expected_django, (
+        f"Django Version Mismatch: {VERSION[: len(expected_django)]} != "
+        f"{expected_django}"
+    )
