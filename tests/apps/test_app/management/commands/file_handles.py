43b84d7f95dfd5b44b36eaf46f498be79d3d087e
Brian Kohan <bckohan@gmail.com>
2025-04-03 10:30:36 -0700

    test for bug described by #209

---
+++ tests/apps/test_app/management/commands/file_handles.py
@@ -0,0 +1,28 @@
+import typing as t
+from django_typer.management import TyperCommand
+from django_typer.completers.path import paths
+import typer
+from pathlib import Path
+
+
+class Command(TyperCommand):
+    help = "Test command for file handles"
+
+    def handle(
+        self,
+        file_text: t.Annotated[typer.FileText, typer.Argument(shell_complete=paths)],
+        file_text_write: typer.FileTextWrite,
+        file_binary: typer.FileBinaryRead,
+        file_binary_write: typer.FileBinaryWrite,
+    ):
+        assert file_text.read().strip() == "FileText"
+        assert file_binary.read().strip() == b"FileBinary"
+
+        file_text_write.write("FileTextWrite")
+        file_binary_write.write(b"FileBinaryWrite")
+
+        file_text_write.close()
+        file_binary_write.close()
+
+        assert Path(file_text_write.name).read_text() == "FileTextWrite"
+        assert Path(file_binary_write.name).read_bytes() == b"FileBinaryWrite"
